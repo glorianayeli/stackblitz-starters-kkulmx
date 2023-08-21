0947fd5fa32e243ea115a877cc29527072b8401b
glorianayeli <42391634+glorianayeli@users.noreply.github.com>
2023-08-20 23:34:34 -0600

    state

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,19 @@
 import { List } from 'exercises/exercise-01';
+import { useState } from 'react';
 
 export default function Index() {
-  return <div></div>;
+  const [elements, addElement] = useState(['xd']);
+
+  function handleClick(e) {
+    addElement([...elements, e.target.value]);
+  }
+  return (
+    <>
+      <input type="text" onClick={handleClick}></input>
+      <button>submit</button>
+      {elements.map((element) => {
+        return <p key={element.title}>{element.title}</p>;
+      })}
+    </>
+  );
 }
