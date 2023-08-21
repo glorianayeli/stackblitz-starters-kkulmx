c460ed0dbf83498973b48504687656c68f4be296
glorianayeli <42391634+glorianayeli@users.noreply.github.com>
2023-08-21 15:42:18 -0600

    2 files modified

--- exercises/exercise-02.tsx
+++ exercises/exercise-02.tsx
@@ -1,7 +1,21 @@
 import { useState } from 'react';
-
+import { List } from '/exercises/exercise-01';
 export default function AddElementToList() {
-  const [elements, AddElement] = useState([]);
+  const [elements, addElement] = useState(['xd']);
+  const [input, setInput] = useState('');
 
-  return <></>;
+  function handleClick() {
+    addElement([...elements, input]);
+  }
+  return (
+    <>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={handleClick}>submit</button>
+      <List elements={elements} />
+    </>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,7 @@
 import { List } from 'exercises/exercise-01';
-import { useState } from 'react';
-
+import { AddElementToList } from 'exercises/exercise-02';
 export default function Index() {
-  const [elements, addElement] = useState(['xd']);
-  const [input, setInput] = useState('');
-
-  function handleClick() {
-    addElement([...elements, input]);
-  }
-  return (
-    <>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></input>
-      <button onClick={handleClick}>submit</button>
-      <List elements={elements}/>
-    </>
-  );
-  FG;
+  <>
+    <AddElementToList />
+  </>;
 }
